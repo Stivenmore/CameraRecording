b9596cbaa921697cd1bce3e9dd49664ccaa758dd
Noah <Ingenierostiven@outlook.es>
2022-09-25 20:30:32 -0500

    video and camera

--- lib/env/prefs.dart
+++ lib/env/prefs.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class UserPreferences {
   static final UserPreferences _instance = UserPreferences._internal();
 
-  factory UserPreferences(){
+  factory UserPreferences() {
     return _instance;
   }
 
@@ -11,19 +11,22 @@ class UserPreferences {
 
   SharedPreferences? _prefs;
 
-  initPrefs()async{
-     this._prefs = await SharedPreferences.getInstance();
+  initPrefs() async {
+    this._prefs = await SharedPreferences.getInstance();
   }
- // logout to app
-  logoutNumber(){
+
+  // logout to app
+  logoutNumber() {
     _prefs!.remove('number');
   }
- // set to token app and save
- set number(String value){
-   _prefs!.setString('number', value);
- }
- // get token
- String get number {
-   return _prefs!.getString('number')?? '';
- }
+
+  // set to token app and save
+  set number(String value) {
+    _prefs!.setString('number', value);
+  }
+
+  // get token
+  String get number {
+    return _prefs!.getString('number') ?? '';
+  }
 }
